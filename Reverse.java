b030955459c88439d92ab17fb15b0ffa247b6d14
Mahathi Muthineni <rajannamuthineni140@gmail.com>
2025-05-03 08:22:25 +0530

    initial commit

---
+++ Reverse.java
@@ -0,0 +1,35 @@
+
+import java.util.*;
+import java.util.Arrays;
+
+public class Reverse {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter number of array elements: ");
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		System.out.println("Enter elements: ");
+		for(int i=0;i<n;i++) {
+			arr[i]=sc.nextInt();
+		}
+		System.out.println("Before Reverse: ");
+		System.out.println(Arrays.toString(arr));
+		int left =0;
+		int right = arr.length-1;
+		while(left<right)
+		{
+			int temp=arr[left];
+			arr[left]=arr[right];
+			arr[right]=temp;
+			left++;
+			right--;
+		}
+		System.out.println("After reverse: ");
+		System.out.println(Arrays.toString(arr));
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
